a503aa889616cd6befc97162d12d34d86f3bd0db
abmantis <amfcalt@gmail.com>
2018-09-06 23:37:14 +0100

    move session from HA

--- setup.py
+++ setup.py
@@ -13,9 +13,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/abmantis/edo_redy_py.git",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'aiohttp',
+        'async_timeout',
+        'datetime'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+
 )
